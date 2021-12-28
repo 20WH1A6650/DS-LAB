4551a05e6e03f8a390b878ad1011a227282b0d5d
20WH1A6650 <93580250+20WH1A6650@users.noreply.github.com>
2021-12-28 11:33:39 +0530

    BST.c

---
+++ Week 5/BST.c
@@ -0,0 +1,114 @@
+
+
+
+/* 
+OUTPUT:
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 75
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 43
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 67
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 89
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 56
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 23
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 22
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 32
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 84
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 100
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 1
+Enter node data: 45
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 2
+INORDER:
+22 23 32 43 45 56 67 75 84 89 100 
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 3
+PREORDER:
+75 43 23 22 32 67 56 45 89 84 100 
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 4
+POSTORDER:
+22 32 23 45 56 67 43 84 100 89 75 
+1.create 
+2.inorder 
+3.preorder 
+4.postorder 
+5.exit
+Enter your choice: 5
+
+*/
